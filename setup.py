152b1b9e51787080f2fd51f14e2d1c90c4656eb6
Shintaro Minami <sminami@LocalMacBook.local>
2025-10-06 17:58:11 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import subprocess
+import setuptools
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfaspr",
+    use_scm_version={'local_scheme': 'no-local-version'},
+    author="Shintaro Minami",
+    description="Japanese Riichi Mahjong Win-Hand Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShintaroMinami/MJHand",
+    ext_modules=[Extension('', [])],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'mahjong',
+        ],
+    scripts=[],
+
+)
